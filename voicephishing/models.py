eff7977b6e568c34e9d743d9d252c224c3a852d9
kingminsu0211 <sleah729@hanyang.ac.kr>
2024-04-11 14:47:33 +0900

    'txt'

---
+++ voicephishing/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from konlpy.tag import Okt
+from glove import Glove
+import numpy as np
+from numpy.linalg import norm
+import pickle
+import re
+
+class Diagnosis(models.Model):
+    DIAGNOSIS_TYPE_CHOICES = [
+        ('통화 녹음본으로 입력하기', '통화 녹음본으로 입력하기'),
+        ('직접 통화 내용 입력하기', '직접 통화 내용 입력하기'),
+    ]
+
+    diagnosis_type = models.CharField(
+        max_length=50,
+        choices=DIAGNOSIS_TYPE_CHOICES,
+    )
+
+    audio_file = models.FileField(upload_to='voice_recordings/',null=True, blank=True)
+    call_details = models.TextField(null=True, blank=True)
+    diagnosis_date = models.DateTimeField(auto_now_add=True)
+    suspicion_percentage = models.FloatField(null=True,blank=True)
+
+    def diagnose(self, audio_transcript=None, call_details=None):
+
+        self.suspicion_percentage = 50
+        self.save()
