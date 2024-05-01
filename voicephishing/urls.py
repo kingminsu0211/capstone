a76ba3e33cae52383752afe59d681b9ab38369f3
kingminsu0211 <sleah729@hanyang.ac.kr>
2024-05-01 11:29:25 +0900

    'add-dignosis'

--- voicephishing/urls.py
+++ voicephishing/urls.py
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     # path('user/', include('user.urls')),
     path('diagnosis/', DiagnosisListView.as_view(), name='diagnosis-list'),
-    # path('voice/', diagnose_voice),
+    path('voice/', diagnose_voice),
 
     # 다른 URL 패턴들을 필요에 따라 추가
 ]
